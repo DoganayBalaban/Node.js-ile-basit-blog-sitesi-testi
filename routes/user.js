cacaa7285d712a8f4966dc8e79f6b48b98663bd3
Doğanay Balaban <dbalaban1907@gmail.com>
2023-03-05 14:14:01 +0300

    on going

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,55 @@
 const express = require("express");
-const router  = express.Router();
-const path    = require("path");
+const router = express.Router();
 
-router.use("/blogs/:blogid",function(req,res){
-    //en özelleştirilmişi en üste al.
-    res.sendFile(path.join(__dirname,"../views/users","blog-details.html"));
+const data = {
+    title: "Popüler Kurslar",
+    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
+    blogs: [
+        {
+            blogid  : 1,
+            baslik  : "Komple Uygulamalı Web Geliştirme",
+            aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            resim   : "1.jpg",
+            anasayfa: true,
+            onay    : true
+        },
+        {
+            blogid  : 2,
+            baslik  : "Node.js",
+            aciklama: "Öğreniyorum.",
+            resim   : "2.jpg",
+            anasayfa: true,
+            onay    : true
+        },
+        {
+            blogid  : 3,
+            baslik  : "HTML,CSS,JS",
+            aciklama: "Nasıl Kullanılır?",
+            resim   : "3.jpeg",
+            anasayfa: true,
+            onay    : true
+        },
+        {
+            blogid  : 3,
+            baslik  : "--------",
+            aciklama: "--------",
+            resim   : "3.jpeg",
+            anasayfa: true,
+            onay    : true
+        },
+    ]
+}
+
+router.use("/blogs/:blogid", function(req, res) {
+    res.render("users/blog-details");
 });
 
-router.use("/blogs",function(req,res){
-    res.sendFile(path.join(__dirname,"../views/users","blogs.html"));
+router.use("/blogs", function(req, res) {
+    res.render("users/blogs", data);
 });
 
-
-router.use("/",function(req,res,next){
-    res.sendFile(path.join(__dirname,"../views/users","index.html"));
+router.use("/", function(req, res) {
+    res.render("users/index", data);
 });
 
 module.exports = router;
